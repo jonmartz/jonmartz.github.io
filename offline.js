405a4e50e5fb75b1528d7d0d2da945234a7ed661
jonmartz <jonmartz91@gmail.com>
2019-03-02 14:18:09 +0200

    First ulpoad

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1551526216,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player-sheet0.png",
+		"images/ride1-sheet0.png",
+		"images/9patch.png",
+		"images/road.png",
+		"images/sprite3-sheet0.png",
+		"images/grass.png",
+		"images/sprite4-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/notebook.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"questions1.txt",
+		"pathfind.js"
+	]
+}
