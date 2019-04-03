95214b4be387d1fe7e5b3a22588b1420aa0e759b
jonmartz <jonmartz91@gmail.com>
2019-04-03 22:24:08 +0200

    Revert "upload 4"
    This reverts commit 81ab01a28f324b137dd0e073f8c501155025f058.

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1554322627,
+	"version": 1554027731,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -37,10 +37,8 @@
 		"images/ride10-sheet0.png",
 		"images/ride10-sheet1.png",
 		"images/ride11-sheet0.png",
-		"images/ride11-sheet1.png",
 		"images/ride12-sheet0.png",
 		"images/ride13-sheet0.png",
-		"images/ride13-sheet1.png",
 		"images/notebookbackground-sheet0.png",
 		"images/tooltipcloud-sheet0.png",
 		"images/tree1-sheet0.png",
@@ -58,12 +56,10 @@
 		"images/fireworkred.png",
 		"images/door-sheet0.png",
 		"images/door-sheet1.png",
-		"images/door-sheet2.png",
 		"images/flag-sheet0.png",
 		"images/spritefont.png",
 		"images/difficultychillis-sheet0.png",
 		"images/connectingline-sheet0.png",
-		"images/questionimage-sheet0.png",
 		"media/footstep1.m4a",
 		"media/footstep1.ogg",
 		"media/footstep2.m4a",
@@ -93,7 +89,6 @@
 		"fractionsnormal.txt",
 		"multiplicationeasy.txt",
 		"כפלeasy.txt",
-		"parallelogram1.png",
 		"pathfind.js"
 	]
 }
